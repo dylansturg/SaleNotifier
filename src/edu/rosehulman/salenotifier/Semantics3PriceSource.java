888c8bba7bc4ab4f79c911e051c1e157d0c6f6aa
Austin Uphus <uphusar@rose-hulman.edu>
2015-01-13 17:23:23 -0500

    Semantic3   - added dependancies for semantic3   - added semantic3 java client code

---
+++ src/edu/rosehulman/salenotifier/Semantics3PriceSource.java
@@ -0,0 +1,16 @@
+package edu.rosehulman.salenotifier;
+
+import java.util.List;
+
+public class Semantics3PriceSource implements IPricingSource {
+
+	private static final String APP_KEY = "SEM336FC88457A9E384669666327917180A7";
+	private static final String SECRET = "NTFmMDBiODZmNmNhNTUxZTEzNWQxODk2OGE5ODM5Mzg";
+	
+	@Override
+	public List<ItemPrice> getPrices(Item item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
