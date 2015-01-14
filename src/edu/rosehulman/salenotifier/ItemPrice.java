dc086fb5f4aa6e0b29e8a5e3e046c76dc1553b6b
Dylan Sturgeon <dylansturg@gmail.com>
2015-01-14 12:31:43 -0500

    adding boilerplate getters/setters and a log tag

--- src/edu/rosehulman/salenotifier/ItemPrice.java
+++ src/edu/rosehulman/salenotifier/ItemPrice.java
@@ -1,15 +1,61 @@
 package edu.rosehulman.salenotifier;
 
-import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class ItemPrice {
+	private long mId;
 	private String productCode;
 	private double price;
-	private Date date;
-	private int sellerId;
+	private GregorianCalendar date;
+	private long sellerId;
 	private IPricingSource source;
-	
-	public Object method(){
+
+	public Object method() {
 		throw new RuntimeException("Don't call this method.");
 	}
+
+	public long getId() {
+		return mId;
+	}
+
+	public void setId(long id) {
+		mId = id;
+	}
+
+	public String getProductCode() {
+		return productCode;
+	}
+
+	public void setProductCode(String upc) {
+		productCode = upc;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double p) {
+		price = p;
+	}
+
+	public GregorianCalendar getDate() {
+		return date;
+	}
+
+	public long getSellerId() {
+		return sellerId;
+	}
+
+	public void setSellerId(long id) {
+		sellerId = id;
+	}
+
+	public IPricingSource getPricingSource() {
+		return source;
+	}
+
+	public void setPricingSource(IPricingSource ps) {
+		source = ps;
+	}
+
 }
--- src/edu/rosehulman/salenotifier/TrackedItemsActivity.java
+++ src/edu/rosehulman/salenotifier/TrackedItemsActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 
 
 public class TrackedItemsActivity extends Activity {
+	
+	public static final String LOG_TAG = "SNL";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
