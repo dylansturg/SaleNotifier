74903d69f01506aba91690b0449c5a4f4c5fbe55
Dylan Sturgeon <dylansturg@gmail.com>
2015-01-16 23:55:15 -0500

    parsed out query on search results page

--- src/edu/rosehulman/salenotifier/SearchResultsActivity.java
+++ src/edu/rosehulman/salenotifier/SearchResultsActivity.java
@@ -1,18 +1,32 @@
 package edu.rosehulman.salenotifier;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class SearchResultsActivity extends Activity {
 	
 	public static final String KEY_SEARCH_ITEM = "KEY_SEARCH_ITEM";
 	public static final String KEY_SEARCH_RADIUS = "KEY_SEARCH_RADIUS";
+	
+	private Item mSearched;
+	private double mSearchRadius;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search_results);
+		
+		Intent launcher = getIntent();
+		mSearched = launcher.getParcelableExtra(KEY_SEARCH_ITEM);
+		mSearchRadius = launcher.getDoubleExtra(KEY_SEARCH_RADIUS, -1);
+		
+		if(mSearched == null){
+			Toast.makeText(this, "Please provide something to search for!", Toast.LENGTH_LONG).show();
+			finish();
+		}
 	}
 }
