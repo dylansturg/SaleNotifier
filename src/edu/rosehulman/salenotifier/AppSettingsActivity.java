ada6a45668cadaec41eda446d87cfbca23b50d16
Dylan Sturgeon <dylansturg@gmail.com>
2015-01-20 16:29:51 -0500

    created activities for settings

---
+++ src/edu/rosehulman/salenotifier/AppSettingsActivity.java
@@ -0,0 +1,15 @@
+package edu.rosehulman.salenotifier;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class AppSettingsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_app_settings);
+	}
+}
---
+++ src/edu/rosehulman/salenotifier/ItemSettingsActivity.java
@@ -0,0 +1,15 @@
+package edu.rosehulman.salenotifier;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ItemSettingsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_item_settings);
+	}
+}
