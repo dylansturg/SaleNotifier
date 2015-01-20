2e5510e270d19e6c9ef05252b74bc8df5653d6ac
Dylan Sturgeon <dylansturg@gmail.com>
2015-01-19 22:06:01 -0500

    initial scanner library code dump

---
+++ src/edu/rosehulman/salenotifier/BarcodeScannerActivity.java
@@ -0,0 +1,15 @@
+package edu.rosehulman.salenotifier;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class BarcodeScannerActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_barcode_scanner);
+	}
+}
