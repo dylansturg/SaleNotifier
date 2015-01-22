4014ea739d4d683d2b6fc366a884fab0bd208cdf
Dylan Sturgeon <dylansturg@gmail.com>
2015-01-22 16:55:06 -0500

    initial item settings layout

--- src/edu/rosehulman/salenotifier/ItemSettingsActivity.java
+++ src/edu/rosehulman/salenotifier/ItemSettingsActivity.java
@@ -9,5 +9,7 @@ public class ItemSettingsActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_item_settings);
+		
+		
 	}
 }
