4f7d49baf1e94a2f0128975beb7a847107dcaea6
Dylan Sturgeon <dylansturg@gmail.com>
2015-02-16 13:57:44 -0500

    using context action menu instead of popup menu - maybe better?

--- src/edu/rosehulman/salenotifier/TrackedItemsActivity.java
+++ src/edu/rosehulman/salenotifier/TrackedItemsActivity.java
@@ -17,15 +17,21 @@ import android.content.Intent;
 import android.net.http.HttpResponseCache;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListView;
 
-public class TrackedItemsActivity extends StorageActivity {
+public class TrackedItemsActivity extends StorageActivity implements
+		OnItemClickListener, OnItemLongClickListener {
 
 	public static final String LOG_TAG = "SNL";
 	protected static final String HTTP_CACHE = "httpCache";
@@ -33,6 +39,9 @@ public class TrackedItemsActivity extends StorageActivity {
 
 	private ListView listView;
 	private TrackedItemsListAdapter listAdapter;
+
+	private ActionMode mActiveActionMode;
+	private TrackedItemsActionMode mActionModeCallback = new TrackedItemsActionMode();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +53,8 @@ public class TrackedItemsActivity extends StorageActivity {
 		listAdapter = new TrackedItemsListAdapter(this, items);
 		listView.setAdapter(listAdapter);
 
-		registerForContextMenu(listView);
+		listView.setOnItemClickListener(this);
+		listView.setOnItemLongClickListener(this);
 
 		// Doesn't create a new alarm if it is already set
 		new SaleNotifierWakefulReceiver().setupRegularAlarm(this);
@@ -72,12 +82,6 @@ public class TrackedItemsActivity extends StorageActivity {
 		}
 	}
 
-	// @Override
-	// public void onBackPressed() {
-	// Item i = new Item("Tea Kettle", "upc", null);
-	// NotificationLauncher.launch(this, i, "Prices Below $20.00");
-	// }
-
 	@Override
 	protected void onResume() {
 		super.onResume();
@@ -85,37 +89,6 @@ public class TrackedItemsActivity extends StorageActivity {
 		List<Item> items = itemSource.getAllItems();
 		listAdapter = new TrackedItemsListAdapter(this, items);
 		listView.setAdapter(listAdapter);
-	}
-
-	@Override
-	public void onCreateContextMenu(ContextMenu menu, View v,
-			ContextMenuInfo menuInfo) {
-		super.onCreateContextMenu(menu, v, menuInfo);
-		getMenuInflater().inflate(R.menu.context_tracked_items, menu);
-	}
-
-	@Override
-	public boolean onContextItemSelected(MenuItem item) {
-		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
-				.getMenuInfo();
-		switch (item.getItemId()) {
-		case R.id.context_tracked_current:
-			launchItemCurrent(info.id);
-			return true;
-		case R.id.context_tracked_history:
-			return true;
-		case R.id.context_tracked_options:
-			launchItemSettings(info.id);
-			return true;
-		case R.id.context_tracked_delete:
-			confirmDeletion(info.id, info.position);
-			return true;
-		case R.id.context_tracked_detailed:
-			launchDetailedItem(info.id);
-			return true;
-		default:
-			return super.onContextItemSelected(item);
-		}
 	}
 
 	@Override
@@ -173,14 +146,16 @@ public class TrackedItemsActivity extends StorageActivity {
 		startActivity(searchIntent);
 	}
 
-	private void confirmDeletion(final long id, final int position) {
+	private void confirmDeletion(final Item deleted) {
+		final long id = deleted.getId();
+		final String name = deleted.getDisplayName();
+
 		DialogFragment deleteDialog = new DialogFragment() {
 			@Override
 			public Dialog onCreateDialog(Bundle savedInstanceState) {
 				AlertDialog.Builder builder = new AlertDialog.Builder(
 						getActivity());
 				builder.setTitle(R.string.dialog_delete_title);
-				String name = listAdapter.getItem(position).getDisplayName();
 				builder.setMessage(getString(R.string.dialog_delete_message,
 						name));
 				builder.setNegativeButton(android.R.string.cancel, null);
@@ -207,5 +182,101 @@ public class TrackedItemsActivity extends StorageActivity {
 		List<Item> items = itemSource.getAllItems();
 		listAdapter = new TrackedItemsListAdapter(this, items);
 		listView.setAdapter(listAdapter);
+	}
+
+	// OnItemClick listener for the tracked items list
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		Item item = (Item) parent.getAdapter().getItem(position);
+		if (!mActionModeCallback.isActive()) {
+			launchItemCurrent(item.getId());
+		} else {
+			mActionModeCallback.setSelected(view, item);
+		}
+	}
+
+	@Override
+	public boolean onItemLongClick(AdapterView<?> parent, View view,
+			int position, long id) {
+		Item selected = (Item) parent.getAdapter().getItem(position);
+		if (mActionModeCallback != null) {
+			mActionModeCallback.setSelected(view, selected);
+			mActiveActionMode = startActionMode(mActionModeCallback);
+		}
+
+		return true;
+	}
+
+	private class TrackedItemsActionMode implements ActionMode.Callback {
+
+		private View mSelectedView;
+		private Item mSelectedItem;
+		private boolean mActive = false;
+
+		public boolean isActive() {
+			return mActive;
+		}
+
+		public void setSelected(View view, Item item) {
+			if (mSelectedView != null) {
+				mSelectedView.setActivated(false);
+			}
+
+			if (view == mSelectedView && mActiveActionMode != null) {
+				mActiveActionMode.finish(); // exit when unselect
+				return;
+			}
+
+			view.setActivated(true);
+			mSelectedView = view;
+			mSelectedItem = item;
+		}
+
+		@Override
+		public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+			getMenuInflater().inflate(R.menu.context_tracked_items, menu);
+			return true;
+		}
+
+		@Override
+		public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+			mActive = true;
+			return false;
+		}
+
+		@Override
+		public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+			switch (item.getItemId()) {
+			case R.id.context_tracked_current:
+				launchItemCurrent(mSelectedItem.getId());
+				break;
+			case R.id.context_tracked_history:
+				break;
+			case R.id.context_tracked_options:
+				launchItemSettings(mSelectedItem.getId());
+				break;
+			case R.id.context_tracked_delete:
+				confirmDeletion(mSelectedItem);
+				break;
+			case R.id.context_tracked_detailed:
+				launchDetailedItem(mSelectedItem.getId());
+				break;
+			default:
+				return false;
+			}
+
+			mode.finish();
+			return true;
+		}
+
+		@Override
+		public void onDestroyActionMode(ActionMode mode) {
+			mActive = false;
+			if (mSelectedView != null) {
+				mSelectedView.setActivated(false);
+			}
+			mActiveActionMode = null;
+		}
 	}
 }
