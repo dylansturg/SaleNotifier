64b7149bf58de7326c039195f267311d7dd610f1
Dylan Sturgeon <dylansturg@gmail.com>
2015-02-16 16:43:12 -0500

    removed imports

--- src/edu/rosehulman/salenotifier/notifications/NotificationLauncher.java
+++ src/edu/rosehulman/salenotifier/notifications/NotificationLauncher.java
@@ -3,7 +3,6 @@ package edu.rosehulman.salenotifier.notifications;
 import java.util.HashMap;
 
 import edu.rosehulman.salenotifier.ItemCurrentActivity;
-import edu.rosehulman.salenotifier.ItemSearchActivity;
 import edu.rosehulman.salenotifier.R;
 import edu.rosehulman.salenotifier.models.Item;
 import android.app.NotificationManager;
@@ -11,7 +10,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
 
 public class NotificationLauncher {
 
@@ -30,18 +28,6 @@ public class NotificationLauncher {
 		Intent resultIntent = new Intent(ctx, ItemCurrentActivity.class);
 		resultIntent.putExtra(ItemCurrentActivity.KEY_ITEM_ID, item.getId());
 
-		// The stack builder object will contain an artificial back stack for
-		// the
-		// started Activity.
-		// This ensures that navigating backward from the Activity leads out of
-		// your application to the Home screen.
-		// TaskStackBuilder stackBuilder = TaskStackBuilder.create(ctx);
-		// // Adds the back stack for the Intent (but not the Intent itself)
-		//
-		// stackBuilder.addParentStack(ItemSearchActivity.class);
-		//
-		// // Adds the Intent that starts the Activity to the top of the stack
-		// stackBuilder.addNextIntent(resultIntent);
 		PendingIntent resultPendingIntent = PendingIntent.getActivity(ctx, 0,
 				resultIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 		mBuilder.setContentIntent(resultPendingIntent);
